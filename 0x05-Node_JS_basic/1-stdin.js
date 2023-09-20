564463d23e28834c0dac098eabef7cbe56e8c642
sezfabian <cheruiyotfabian@gmail.com>
2023-09-20 20:16:52 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,12 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on("data", data => {
-  data = data.toString();
-  process.stdout.write("Your name is: " + data);
-  process.stdout.write("This important software is now closing\n");
-  process.exit();
+process.stdin.on("readable", () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write("Your name is: " + name);
+  }
 });
+
+process.stdin.on("end", () => {
+  process.stdout.write('This important software is now closing\n');
+})
