ae72be7586326963062a954f7f6a48e1845e0dd8
sezfabian <cheruiyotfabian@gmail.com>
2023-09-20 21:20:20 +0300

    reading sync

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const file = fs.readFileSync(path, 'utf-8');
+    const lines = file.split('\n');
+    const count = lines.length - 1;
+    const CSlist = [];
+    const SWElist = [];
+
+    for (const line of lines) {
+      const student = line.split(',');
+      if (student[3] === 'CS') {
+        CSlist.push(` ${student[0]}`);
+      }
+      if (student[3] === 'SWE') {
+        SWElist.push(` ${student[0]}`);
+      }
+    }
+    console.log(`Number of students: ${count}`);
+    console.log(`Number of CS students: ${CSlist.length}. List: ${CSlist}`);
+    console.log(`Number of SWE students: ${SWElist.length}. List:${SWElist}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
