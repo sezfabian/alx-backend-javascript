05abf1638bd793e0a24faf2a1d6809bd089922f1
sezfabian <cheruiyotfabian@gmail.com>
2023-09-20 20:39:04 +0300

    es-lint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write('Your name is: ' + name);
+  const userName = process.stdin.read();
+  if (userName !== null) {
+    process.stdout.write(`Your name is: ${userName}`);
   }
 });
 
-process.stdin.on("end", () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
