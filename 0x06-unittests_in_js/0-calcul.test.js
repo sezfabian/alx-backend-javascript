999b26d0e5a7bd34eb6d80cd4a4e82b846127e21
sezfabian <cheruiyotfabian@gmail.com>
2023-09-22 04:23:33 +0300

    tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,18 +1,24 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  const testCalculateNumber = ({a, b, expected}) => {
-    const result = calculateNumber(a, b);
-    assert.equal(result, expected);
- };
+describe('calculateNumber', () => {
+  it('correctly adds two numbers', () => {
+    assert.equal(calculateNumber(1, -2), -1);
+  });
 
-  it(`passes when add 1 and 2`, testCalculateNumber({a: 1, b: 2, expected: 3}));
-  it(`passes add 1.3 and 3.7`, testCalculateNumber({a: 1.3, b: 3.7, expected: 5}));
-  it(`passes add 1 and 3.7`, testCalculateNumber({a: 1, b: 3.7, expected: 5}));
-  it(`passes add 1.3 and 3`, testCalculateNumber({a: 1.3, b: 3, expected: 4}));
-  it(`passes add 1 and -2`, testCalculateNumber({a: 1, b: -2, expected: -1}));
-  it(`passes add 10000 and 200000`, testCalculateNumber({a: 10000, b: 200000, expected: 210000}));
-  it(`passes add -1.3 and 3.8`, testCalculateNumber({a: -1.3, b: 3.7, expected: 3}));
-  it(`passes add -1 and -2`, testCalculateNumber({a: -1, b: -2, expected: -3}));
+  it('correctly adds two negative numbers', () => {
+    assert.equal(calculateNumber(-1, -2), -3);
+  });
+
+  it('correctly adds two positive numbers', () => {
+    assert.equal(calculateNumber(1, 2), 3);
+  });
+
+  it('correctly adds two floating point numbers', () => {
+    assert.equal(calculateNumber(1.7, 3.2), 5);
+  });
+
+  it('correctly adds two large positive numbers', () => {
+    assert.equal(calculateNumber(10000000, 20000000), 30000000);
+  });
 });
