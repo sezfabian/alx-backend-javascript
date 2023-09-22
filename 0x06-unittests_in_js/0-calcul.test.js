1db616076a9ef1056f526057ff3bd4dc5a5676a6
sezfabian <cheruiyotfabian@gmail.com>
2023-09-22 04:28:53 +0300

    tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,36 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
+describe('calculateNumber', function() {
   it('correctly adds two numbers', () => {
-    assert.equal(calculateNumber(1, -2), -1);
+    assert.strictEqual(calculateNumber(1.0, -2.0), -1);
   });
 
   it('correctly adds two negative numbers', () => {
-    assert.equal(calculateNumber(-1, -2), -3);
+    assert.strictEqual(calculateNumber(-1, -2), -3);
   });
 
   it('correctly adds two positive numbers', () => {
-    assert.equal(calculateNumber(1, 2), 3);
+    assert.strictEqual(calculateNumber(1, 2), 3);
   });
 
   it('correctly adds two floating point numbers', () => {
-    assert.equal(calculateNumber(1.7, 3.2), 5);
+    assert.strictEqual(calculateNumber(1.7, 3.2), 5);
   });
 
   it('correctly adds two large positive numbers', () => {
-    assert.equal(calculateNumber(10000000, 20000000), 30000000);
+    assert.strictEqual(calculateNumber(10000000, 20000000), 30000000);
+  });
+
+  it('correctly rounds up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
+
+  it('correctly rounds up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('correctly rounds down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
